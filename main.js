fde336e0e184e2d85707b9bc91fc35e05218f4e5
Denis <48333782+dkanukov@users.noreply.github.com>
2022-01-13 19:07:39 +0300

    create basic layout with js

---
+++ main.js
@@ -0,0 +1,54 @@
+/* esversion 6 */
+(function(){
+
+	//create and return header of app
+	function createAppTitle(title) {
+		let appTitle = document.createElement('h2');
+		appTitle.innerHTML = title;
+		return appTitle;
+	}
+
+	//create and return form for task
+	function createTodoItemForm() {
+		let form = document.createElement('form');
+		let input = document.createElement('input');
+		let buttonWrapper = document.createElement('div');
+		let	button = document.createElement('button');
+
+		form.classList.add('input-group', 'mb-3');
+		input.classList.add('form-control');
+		input.placeholder =  'Введите название нового дела';
+		buttonWrapper.classList.add('input-group-append');
+		button.classList.add('btn', 'btn-primary');
+		button.textContent = 'Добавить дело';
+
+		buttonWrapper.append(button);
+		form.append(input);
+		form.append(buttonWrapper);
+
+		return {
+			form,
+			input,
+			button
+		};
+	}
+
+	//create and return list of elements
+	function createTodoList() {
+		let list = document.createElement('ul');
+		list.classList.add('list-group');
+		return list;
+	}
+
+	document.addEventListener('DOMContentLoaded', function() {
+		let container = document.getElementById('todo-app');
+
+		let todoAppTitle = createAppTitle('Список дел');
+		let todoItemForm = createTodoItemForm();
+		let	todoList = createTodoList();
+
+		container.append(todoAppTitle);
+		container.append(todoItemForm.form);
+		container.append(todoList);
+	});
+})();
